71a690d7b5136e98dd3cb6cc8b915a923d94db6a
AmirabbasJ <amirabbasgamer2@gmail.com>
2021-01-03 14:12:50 +0330

    feat: add mongoose puzzle schema ✨

---
+++ src/models/mongoose/schema/puzzle.schema.ts
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import mongoose from 'mongoose';
+
+import TestSchema from './test.schema';
+
+const PuzzleSchema = new mongoose.Schema(
+	{
+		_id: String,
+		title: String,
+		fastest: Boolean,
+		reverse: Boolean,
+		shortest: Boolean,
+		statement: String,
+		testCases: [TestSchema],
+		constraints: String,
+		stubGenerator: String,
+		inputDescription: String,
+		outputDescription: String,
+	},
+	{ _id: false }
+);
+
+export default PuzzleSchema;
---
+++ src/models/mongoose/schema/test.schema.ts
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+
+const TestSchema = new mongoose.Schema({
+	title: String,
+	isTest: Boolean,
+	testIn: String,
+	testOut: String,
+	isValidator: Boolean,
+});
+
+export default TestSchema;
