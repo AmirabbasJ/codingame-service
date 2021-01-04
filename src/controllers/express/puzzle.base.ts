96a587699e9d4afd2ada1a0cbb23d3fa96913d4f
AmirabbasJ <amirabbasgamer2@gmail.com>
2021-01-04 12:12:32 +0330

    refactor: use RequestHandler instead of explicit typing ♻️

--- src/controllers/express/puzzle.base.ts
+++ src/controllers/express/puzzle.base.ts
@@ -1,20 +1,12 @@
-import { Request, Response, NextFunction } from 'express';
+import { RequestHandler } from 'express';
 import PuzzleRepo from '../../repository/base/puzzle.base';
 
 import PuzzleController from '../base/puzzle.base';
 
 interface ExpressPuzzleController extends PuzzleController {
 	puzzleRepo: PuzzleRepo;
-	getRandomPuzzleHandler: (
-		req: Request,
-		res: Response,
-		next: NextFunction
-	) => Promise<void>;
-	getPuzzleByIdHandler: (
-		req: Request,
-		res: Response,
-		next: NextFunction
-	) => Promise<void>;
+	getRandomPuzzleHandler: RequestHandler;
+	getPuzzleByIdHandler: RequestHandler;
 }
 
 export default ExpressPuzzleController;
